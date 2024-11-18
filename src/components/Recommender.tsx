5443ba4e4b3504434663b384baaed28ae1996433
Pontus Grandin <pontusgrandin6@gmail.com>
2024-11-18 18:27:58 +0100

    ui for pearson/euclidean requests

--- src/components/Recommender.tsx
+++ src/components/Recommender.tsx
@@ -4,6 +4,7 @@ import { FormEvent, useState } from "react";
 import FormParagraph from "./FormParagraph";
 import DataTable from "./DataTable";
 import ErrorMessage from "./ErrorMessage";
+import Selector from "./Selector";
 
 export declare type User = {
   UserId: number;
@@ -11,10 +12,15 @@ export declare type User = {
 };
 
 declare type reccType = "item-based" | "user-based";
+declare type simType = "Euclidean" | "Pearson";
 
 const recommendationTypes: { itemBased: reccType; userBased: reccType } = {
   itemBased: "item-based",
   userBased: "user-based",
+};
+const simTypes: { pearson: simType; euclidean: simType } = {
+  pearson: "Pearson",
+  euclidean: "Euclidean",
 };
 
 const headers = ["ID", "Title", "Score"];
@@ -25,27 +31,41 @@ const Recommender = ({ users }: { users: User[] }) => {
     recommendationTypes.itemBased,
   );
   const [results, setResults] = useState("");
+
   const [recommendations, setRecommendations] = useState<
     Recommendation[] | null
   >(null);
+  const [simType, setSimType] = useState<simType>(simTypes.euclidean);
+
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    // Ensure user input is correct
     if (results.length === 0 || Number(results) < 1) {
       setErrorMessage("Please input a number bigger than 0.");
       return;
     }
+    if (
+      reccType === recommendationTypes.itemBased &&
+      simType === simTypes.pearson
+    ) {
+      setErrorMessage("Pearson is only available for user-based comparisons");
+      return;
+    }
 
-    const params = new URLSearchParams({
-      user: selectedUser,
-      results: results,
-    }).toString();
-
+    // Build the request
     const endpoint =
       reccType === recommendationTypes.itemBased
         ? "/api/item-recommend"
         : "/api/user-recommend";
+
+    const params = new URLSearchParams({
+      user: selectedUser,
+      results: results,
+      simType: simType,
+    }).toString();
+
     const response = await fetch(`${endpoint}?${params}`);
     if (response.ok) {
       const data = await response.json();
@@ -56,31 +76,32 @@ const Recommender = ({ users }: { users: User[] }) => {
       setErrorMessage("Something went wrong when fetching the data");
     }
   };
+
   return (
     <div>
       <div className="mt-4 gap-2">
         <form onSubmit={handleSubmit} className="flex flex-col items-center">
           <div className="flex items-center justify-center flex-row">
             <FormParagraph text="User: " />
-            <select
-              className="ml-2 pl-2 border p-1"
-              onChange={(e) => setSelectedUser(e.target.value)}
-            >
+            <Selector<string> setState={setSelectedUser}>
               {users.map((user) => (
                 <option key={user.UserId} value={user.UserId}>
                   {user.UserId + ". " + user.Name}
                 </option>
               ))}
-            </select>
+            </Selector>
 
             <FormParagraph text="Type: " />
-            <select
-              className="ml-2 pl-2 border p-1"
-              onChange={(e) => setReccType(e.target.value as reccType)}
-            >
+            <Selector<reccType> setState={setReccType}>
               <option value={recommendationTypes.itemBased}>Item Based</option>
               <option value={recommendationTypes.userBased}>User Based</option>
-            </select>
+            </Selector>
+
+            <FormParagraph text="Similarity" />
+            <Selector<simType> setState={setSimType}>
+              <option value={recommendationTypes.itemBased}>Euclidean</option>
+              <option value={recommendationTypes.userBased}>Pearson</option>
+            </Selector>
 
             <FormParagraph text="Results: " />
             <input
